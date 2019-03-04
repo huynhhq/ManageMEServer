d79e788ef0c59c7a2cad4be3399fa75f766b6c34
huynhhq96 <huynhhq96@gmail.com>
2019-03-04 08:28:00 +0700

    wip: Add EF Reverse POCO

---
+++ ManageMEServer/ClassMapping/AppInfoConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ManageMEServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Mapping;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Web;
+
+namespace ManageMEServer.ClassMapping
+{
+	public class AppInfoConfiguration : EntityTypeConfiguration<AppInfo>	 
+	{
+		public AppInfoConfiguration()
+		{
+			ToTable("tblAppInfo");
+		}
+	}
+}
---
+++ ManageMEServer/Data/ManageMeContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ManageMEServer.Data
+{
+	public class ManageMeContext : DbContext
+	{
+		public System.Data.Entity.DbSet<AppInfo> AppInfoes { get; set; } // AppInfoes
+		public System.Data.Entity.DbSet<User> Users { get; set; } // Users
+		public System.Data.Entity.DbSet<UserConfig> UserConfigs { get; set; } // UserConfigs
+		public System.Data.Entity.DbSet<UserPhoneInfo> UserPhoneInfoes { get; set; } // UserPhoneInfoes
+
+		protected override void OnModelCreating(System.Data.Entity.DbModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Configurations.Add(new AppInfoConfiguration());
+			modelBuilder.Configurations.Add(new UserConfiguration());
+			modelBuilder.Configurations.Add(new UserConfigConfiguration());
+			modelBuilder.Configurations.Add(new UserPhoneInfoConfiguration());
+		}
+	}
+	
+}
